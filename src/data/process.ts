387d7e989a14c798227738aa6514a44e8790151f
Ayush kumar <kumarayushkumar@protonmail.com>
2024-11-04 17:57:29 +0530

    Update README and add project process documentation

---
+++ src/data/process.ts
@@ -0,0 +1,139 @@
+const process = [
+  {
+    title: 'Project Brief',
+    description:
+      'Start by defining the project’s purpose and scope, focusing on understanding the target audience and establishing a clear vision.'
+  },
+  {
+    title: 'Site Map',
+    description:
+      'Create a site map to organize and structure the website’s pages, showing how users will navigate through the content. '
+  },
+  {
+    title: 'Wire frame',
+    description:
+      'Sketch out a basic layout on paper, focusing on the structure without any design details.'
+  },
+  {
+    title: 'Mood board',
+    description:
+      'Now look around to find the inspiration, gather visual inspirations—colors, textures, fonts, and images—that align with the brand’s feel. Using convergent thinking to refine ideas and divergent thinking to explore creative options.'
+  },
+  {
+    title: 'Brand kit',
+    description:
+      'Develop a consistent brand identity by defining core elements: a color palette that reflects the brand’s personality, typography choices for readability and style, a logo for brand recognition, and feature photos that capture the brand’s essence. This kit serves as the foundation for all visual elements in the design.',
+    elements: {
+      color: {
+        how: [
+          {
+            link: 'https://www.empower-yourself-with-color-psychology.com/meaning-of-colors.html'
+          },
+          {
+            link: 'https://careerfoundry.com/en/blog/ui-design/introduction-to-color-theory-and-color-palettes/'
+          },
+          { link: 'https://www.toptal.com/designers/ux/color-in-ux' },
+          {
+            link: 'https://londonimageinstitute.com/how-to-empower-yourself-with-color-psychology/'
+          }
+        ],
+        where: [
+          { link: 'https://palettemaker.com/' },
+          { link: 'https://www.rapidtables.com/web/css/css-color.html' },
+          { link: 'https://www.happyhues.co/palettes/4' },
+          { link: 'https://colorhunt.co/' },
+          { link: 'https://mycolor.space/' },
+          { link: 'https://color.adobe.com/create/color-wheel' },
+          { link: 'https://colors.muz.li/' },
+          { link: 'https://webgradients.com/' }
+        ]
+      },
+      typography: {
+        how: [
+          { link: 'https://www.canva.com/learn/typography-design/' },
+          {
+            link: 'https://uxplanet.org/7-things-to-remember-when-selecting-fonts-for-your-design-ec1e592266c5'
+          }
+        ],
+        where: [
+          { link: 'https://textfonts.net/' },
+          { link: 'https://www.fontbrief.com/fontbrief' },
+          { link: 'https://fontforge.org/en-US/' },
+          { link: 'https://typescale.com/' },
+          { link: 'https://www.fontsquirrel.com/' },
+          { link: 'https://www.dafont.com/' },
+          { link: 'https://www.fontspace.com/' },
+          { link: 'https://www.myfonts.com/WhatTheFont/' },
+          { link: 'https://www.fontspring.com/' },
+          { link: 'https://www.fontshop.com/' },
+          { link: 'https://www.fonts.com/' }
+        ]
+      },
+      logo: {
+        how: [
+          { link: 'https://www.logolounge.com/' },
+          { link: 'https://www.logodesignlove.com/' }
+        ],
+        where: [
+          { link: 'https://ideogram.ai/login' },
+          { link: 'https://logodiffusion.com/' }
+        ]
+      }
+    }
+  },
+  {
+    title: 'UI Design',
+    description: 'Start designing the interface in figma or any tool you like.',
+    elements: {
+      images: {
+        where: [
+          { link: 'https://unsplash.com/' },
+          { link: 'https://www.pexels.com/' },
+          { link: 'https://pixabay.com/' }
+        ]
+      },
+      illustrations: {
+        where: [
+          { link: 'https://undraw.co/' },
+          { link: 'https://www.drawkit.io/' },
+          { link: 'https://www.humaaans.com/' },
+          { link: 'https://openpeeps.com/' },
+          { link: 'https://onfire.craftwork.design' },
+          { link: 'https://www.reshot.com/' },
+          { link: 'https://blush.design/' },
+          { link: 'https://haikei.app/' },
+          { link: 'http://getwaves.io', comment: 'Waves images' },
+          { link: 'https://manypixels.co/', comment: 'Free illustration' },
+          { link: 'https://patternkid.com/', comment: 'Pattern' },
+          { link: 'https://www.patterned.ai/', comment: 'Pattern' }
+        ]
+      },
+      icons: {
+        where: [
+          { link: 'https://iconmonstr.com/' },
+          { link: 'https://www.iconshock.com/' },
+          { link: 'https://www.iconsdb.com/' },
+          { link: 'https://www.iconspedia.com/' },
+          { link: 'https://www.iconarchive.com/' },
+          { link: 'https://www.iconscout.com/' },
+          { link: 'https://www.icons8.com/' },
+          { link: 'https://www.flaticon.com/' },
+          { link: 'https://tablericons.com/' },
+          { link: 'https://thenounproject.com/' },
+          { link: 'https://heroicons.com/' },
+          { link: 'https://ionic.io/ionicons' },
+          { link: 'https://icons.theforgesmith.com' },
+          { link: 'https://iconset.io/icons' }
+        ]
+      }
+    }
+  },
+  {
+    title: 'Responsive Design',
+    description: 'Make it responsive.'
+  },
+  {
+    title: 'Development',
+    description: 'Write the code.'
+  }
+]
