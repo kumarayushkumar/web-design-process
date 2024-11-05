bf08af37d804b8aaff4ed0c3234fb08b7323f698
Ayush kumar <kumarayushkumar@protonmail.com>
2024-11-05 17:38:47 +0530

    Add Vercel adapter and web analytics integration to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import vercelServerless from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-})
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercelServerless({
+    webAnalytics: { enabled: true }
+  }),
+});
