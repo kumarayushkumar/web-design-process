caaa388feeda2c10a68614ef9ee629e70ec0d5c9
Ayush kumar <kumarayushkumar@protonmail.com>
2025-01-04 12:42:32 +0530

    Add sitemap integration and update sitemap references in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import vercelServerless from '@astrojs/vercel/serverless';
+
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercelServerless({
-    webAnalytics: { enabled: true }
-  }),
-});
+  site: 'https://uidesignprocess.tech',
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: 'daily',
+    })
+  ]
+})
